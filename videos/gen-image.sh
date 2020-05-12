8d9e93dedfd5a116ecf07f362c3a5d78b18e773f
Prima Wirawan <primailkom@gmail.com>
2020-05-12 14:20:32 +0700

    Remove images and videos from gitignore

---
+++ videos/gen-image.sh
@@ -0,0 +1,17 @@
+ffmpeg -ss 00:05 -i '11.1- Introduction to Chrome Extensions - Programming with Text - YouTube.MP4' -frames:v 1 '11.1- Introduction to Chrome Extensions - Programming with Text - YouTube.png'
+
+ffmpeg -ss 00:05 -i '11.2- Bookmarklets - Programming with Text - YouTube.MP4' -frames:v 1 '11.2- Bookmarklets - Programming with Text - YouTube.png'
+
+ffmpeg -ss 00:05 -i '11.2- Bookmarklets - Programming with Text - YouTube.MP4' -frames:v 1 '11.2- Bookmarklets - Programming with Text - YouTube.png'
+
+ffmpeg -ss 00:05 -i '11.3- Chrome Extensions- Content Scripts - Programming with Text - YouTube.MP4' -frames:v 1 '11.3- Chrome Extensions- Content Scripts - Programming with Text - YouTube.png'
+
+ffmpeg -ss 00:05 -i '11.4- Chrome Extensions- Background Scripts - Programming with Text - YouTube.MP4' -frames:v 1 '11.4- Chrome Extensions- Background Scripts - Programming with Text - YouTube.png'
+
+ffmpeg -ss 00:05 -i '11.5- Chrome Extensions- Pop-ups - Programming with Text - YouTube.MP4' -frames:v 1 '11.5- Chrome Extensions- Pop-ups - Programming with Text - YouTube.png'
+
+ffmpeg -ss 00:05 -i '11.6- Chrome Extensions- Pop-ups Messaging - Programming with Text - YouTube.MP4' -frames:v 1 '11.6- Chrome Extensions- Pop-ups Messaging - Programming with Text - YouTube.png'
+
+ffmpeg -ss 00:05 -i '11.7- Chrome Extensions- New Tab Override - Programming with Text - YouTube.MP4' -frames:v 1 '11.7- Chrome Extensions- New Tab Override - Programming with Text - YouTube.png'
+
+ffmpeg -ss 00:05 -i '11.8- Cross-Browser Extensions - Programming with Text - YouTube.MP4' -frames:v 1 '11.8- Cross-Browser Extensions - Programming with Text - YouTube.png'
